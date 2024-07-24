178514fdb6687f5d6f71c37547f8b6dd0aec8ee8
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-07-24 13:52:08 -0300

    Feat : create trip and controller

---
+++ src/main/java/br/com/marques/planner/participant/ParticipantService.java
@@ -0,0 +1,13 @@
+package br.com.marques.planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+    public void registerParticipantToEvent(List<String> participantsToInvite, UUID id) {}
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId) {}
+}
